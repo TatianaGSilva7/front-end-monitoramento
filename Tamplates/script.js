1b77f2f790cee47f6d3c9ea5706997c9adedea2f
VictorLemosbar <victor.escolas9045@gmail.com>
2026-03-26 19:47:39 -0300

    Começando fazer o js

--- Tamplates/script.js
+++ Tamplates/script.js
@@ -2,3 +2,25 @@
 function alerta() {
     alert("Olá, você clicou no botão!");
 }
+
+
+async function getCode1() {
+    const url = ""
+    try {
+        fetch(url)
+            .then(response => response.json)
+    } catch(err) {
+        alert("Não foi possivel pegar os dados")
+    }
+}
+
+const qrcode = new QRCode("qrcode1")
+
+function criarCode() {
+    const elText = document.getElementById("text");
+
+
+}
+
+
+new QRCode(document.getElementById("qrcode1"), "http://jindo.dev.naver.com/collie");
